53220c6195cdd0a44e189a609c459a8044db614f
Antwan Foster <antwan@fostercreativelabs.com>
2017-07-18 00:40:49 -0400

    Initial component structure

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-export default class App extends Component {
+class App extends Component {
   render() {
     return (
       <div className="App">
@@ -18,4 +18,4 @@ export default class App extends Component {
   }
 }
 
-// export default App;
+export default App;
